955779830f3bd8e95aaa9fe4f2e1df7459871de3
leeyji95 <leeyji95@naver.com>
2020-07-18 17:51:41 +0900

    인사관리 테이블 컬럼 수정 및 추가

--- src/main/webapp/ERD/create_table.sql
+++ src/main/webapp/ERD/create_table.sql
@@ -4,10 +4,11 @@ DROP TABLE tb_evidence_file CASCADE CONSTRAINT purge;
 DROP TABLE tb_account CASCADE CONSTRAINT purge;
 
 DROP TABLE tb_dept CASCADE CONSTRAINT purge;
-DROP TABLE tb_emp CASCADE CONSTRAINT purge;
 DROP TABLE tb_position CASCADE CONSTRAINT purge;
+DROP TABLE tb_emp CASCADE CONSTRAINT purge;
 DROP TABLE tb_commute CASCADE CONSTRAINT purge;
 DROP TABLE tb_apply CASCADE CONSTRAINT purge;
+DROP TABLE tb_password CASCADE CONSTRAINT purge;
 
 DROP TABLE tb_publisher CASCADE CONSTRAINT purge;
 DROP TABLE tb_book CASCADE CONSTRAINT purge;
@@ -27,6 +28,9 @@ DROP SEQUENCE statement_seq;
 DROP SEQUENCE dept_seq;
 DROP SEQUENCE position_seq;
 DROP SEQUENCE emp_seq;
+DROP SEQUENCE pw_seq;
+DROP SEQUENCE commute_seq;
+DROP SEQUENCE apply_seq;
 
 DROP SEQUENCE publisher_seq;
 DROP SEQUENCE book_seq;
@@ -45,7 +49,13 @@ CREATE SEQUENCE statement_seq;
 
 CREATE SEQUENCE dept_seq;
 CREATE SEQUENCE position_seq;
-CREATE SEQUENCE emp_seq;
+CREATE SEQUENCE pw_seq;
+CREATE SEQUENCE commute_seq;
+CREATE SEQUENCE apply_seq;
+CREATE SEQUENCE emp_seq
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 9999;
 
 CREATE SEQUENCE publisher_seq;
 CREATE SEQUENCE book_seq;
@@ -75,75 +85,57 @@ SELECT * FROM tb_position ;
 
 CREATE TABLE tb_emp
 (
-    emp_uid              NUMBER           NOT NULL, 
-    emp_number           NUMBER           NOT NULL, 
-    emp_name             VARCHAR2(20)     NOT NULL, 
-    emp_email            VARCHAR2(30)     NOT NULL, 
-    dept_uid             NUMBER           NOT NULL, 
-    position_uid         NUMBER           NOT NULL, 
-    emp_pw               VARCHAR2(200)    DEFAULT 'emp_uid' NOT NULL, 
-    emp_phone            VARCHAR2(15)     NOT NULL, 
-    emp_hiredate         DATE             DEFAULT SYSDATE NOT NULL, 
-    emp_endday           DATE             NULL, 
-    emp_annualholiday    NUMBER           NULL, 
-    emp_holidaycount     NUMBER           NULL, 
-    emp_admin            NUMBER           NOT NULL, 
-    CONSTRAINT TB_EMP_PK PRIMARY KEY (emp_uid)
+    emp_uid         NUMBER          NOT NULL PRIMARY KEY, 
+    emp_number      NUMBER          NOT NULL UNIQUE , 
+    emp_name        VARCHAR2(20)    NOT NULL, 
+    emp_email       VARCHAR2(30)    NOT NULL UNIQUE, 
+    dept_uid        NUMBER          NOT NULL REFERENCES tb_dept (dept_uid), 
+    position_uid    NUMBER          NOT NULL REFERENCES tb_position (position_uid), 
+    emp_phone       VARCHAR2(15)    NOT NULL, 
+    emp_hiredate    DATE            DEFAULT SYSDATE NOT NULL, 
+    emp_admin       CHAR(1)         DEFAULT 'N' NOT NULL
 );
+
+INSERT INTO TB_EMP VALUES (emp_seq.nextval, CONCAT(TO_CHAR(sysdate, 'YYMM'), LPAD(emp_seq.nextval, 4, 0)) , '이예지', 'rlaaudwk@daum.com', 5, 5, 'EMP_NUMBER','01089785645', SYSDATE, 0 );
+INSERT INTO TB_EMP VALUES (emp_seq.nextval, CONCAT(TO_CHAR(sysdate, 'YYMM'), LPAD(emp_seq.nextval, 4, 0)) , '김예진', 'RLAAUDWLS@daum.com', 6, 7, 'eDFjke', '01089743645', TO_CHAR(SYSDATE, 'YYYY-MM-DD'), 0 );
+-- 사원번호 완성
+SELECT CONCAT(TO_CHAR(sysdate, 'YYMM'), LPAD(emp_seq.nextval, 4, 0)) FROM DUAL; 
+
+DELETE FROM TB_EMP ;
 SELECT * FROM tb_emp ;
 
-ALTER TABLE tb_emp
-    ADD CONSTRAINT FK_tb_emp_position_uid_tb_posi FOREIGN KEY (position_uid)
-        REFERENCES tb_position (position_uid);
-       
-ALTER TABLE tb_emp
-    ADD CONSTRAINT FK_tb_emp_dept_uid_tb_dept_dep FOREIGN KEY (dept_uid)
-        REFERENCES tb_dept (dept_uid);
-
-ALTER TABLE tb_emp
-    ADD CONSTRAINT UC_emp_number UNIQUE (emp_number);
-
-ALTER TABLE tb_emp
-    ADD CONSTRAINT UC_emp_email UNIQUE (emp_email);
-
-
+CREATE TABLE tb_password
+(
+    pw_uid       NUMBER           NOT NULL PRIMARY KEY, 
+    pw_origin    VARCHAR2(200)    NOT NULL, 
+    pw_change    VARCHAR2(200)    NOT NULL, 
+    emp_uid      NUMBER           NOT NULL REFERENCES tb_emp (emp_uid)
+);
 
 CREATE TABLE tb_commute
 (
-    commute_uid         NUMBER          NOT NULL, 
-    commute_state       VARCHAR2(10)    NOT NULL, 
-    commute_date        DATE            DEFAULT SYSDATE NOT NULL, 
-    emp_uid             NUMBER          NOT NULL, 
+    commute_uid         NUMBER          NOT NULL PRIMARY KEY, 
+    emp_uid             NUMBER          NOT NULL REFERENCES tb_emp (emp_uid), 
+    commute_date        DATE            DEFAULT SYSDATE NULL, 
     commute_start       TIMESTAMP       NULL, 
-    commute_total       NUMBER          NULL, 
     commute_end         TIMESTAMP       NULL, 
     commute_overtime    NUMBER          NULL, 
-    CONSTRAINT TB_COMMUTE_PK PRIMARY KEY (commute_uid)
+    commute_total       NUMBER          NULL, 
+    commute_state       VARCHAR2(10)    NOT NULL, 
+    commute_is_apply    NUMBER          DEFAULT 0 NULL
 );
-SELECT * FROM tb_commute ;
-
-ALTER TABLE tb_commute
-    ADD CONSTRAINT FK_tb_commute_emp_uid_tb_emp_e FOREIGN KEY (emp_uid)
-        REFERENCES tb_emp (emp_uid);
 
    
 CREATE TABLE tb_apply
 (
-    apply_uid            NUMBER    NOT NULL, 
-    apply_type           NUMBER    NOT NULL, 
-    emp_uid              NUMBER    NOT NULL, 
-    apply_workdate       DATE      NOT NULL, 
-    apply_holidaydate    DATE      NULL, 
-    apply_extra          NUMBER    NULL, 
-    is_apply             NUMBER    NULL, 
-    CONSTRAINT TB_APPLY_PK PRIMARY KEY (apply_uid)
+    apply_uid            NUMBER           NOT NULL PRIMARY KEY, 
+    emp_uid              NUMBER           NOT NULL REFERENCES tb_emp (emp_uid), 
+    apply_date           DATE             DEFAULT SYSDATE NOT NULL, 
+    apply_holiyperiod    DATE             NULL, 
+    apply_extra          NUMBER           NULL, 
+    apply_outplace       VARCHAR2(200)    NULL, 
+    commute_uid          NUMBER           NOT NULL REFERENCES tb_commute (commute_uid)
 );
-SELECT * FROM tb_apply ;
-
-ALTER TABLE tb_apply
-    ADD CONSTRAINT FK_tb_apply_emp_uid_tb_emp_emp FOREIGN KEY (emp_uid)
-        REFERENCES tb_emp (emp_uid);
-       
        
        
 -- 계정과목 테이블      
@@ -265,11 +257,6 @@ CREATE TABLE tb_attach
 ALTER TABLE tb_attach
     ADD CONSTRAINT FK_tb_attach_book_uid_tb_book_ FOREIGN KEY (book_uid)
         REFERENCES tb_book (book_uid);
-
-       
-
-
-
    
        
 CREATE TABLE tb_order
